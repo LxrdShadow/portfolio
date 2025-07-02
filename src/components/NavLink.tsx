07607bb848b32e52feb7843bdbcc11cba6c9a28d
LxrdShadow <andritiana.idealy@gmail.com>
2025-07-03 00:59:18 +0300

    feat(nav): add nav link navigation

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,11 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 type Props = {
     title: string;
     icon?: React.ReactNode;
+    href: string;
 };
 
-function NavLink({ title, icon }: Props) {
+function NavLink({ title, icon, href }: Props) {
+    const router = useRouter();
+
     return (
-        <a className="hover:underline text-txt-light dark:text-txt-dark flex flex-row items-center gap-2 cursor-pointer">
+        <a
+            className="hover:underline text-txt-light dark:text-txt-dark flex flex-row items-center gap-2 cursor-pointer"
+            onClick={() => router.push(href)}
+        >
             {icon}
             <p>{title}</p>
         </a>
--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -8,6 +8,8 @@ import {
     LaptopMinimal,
     Moon,
     Sun,
+    X,
+    Menu,
 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -20,6 +22,7 @@ export default function AppLayout({
 }>) {
     const { ref, toggleSwitchTheme, isDarkMode } = useModeAnimation();
     const router = useRouter();
+    const [isOpen, setIsOpen] = useState(false);
 
     const [hasMounted, setHasMounted] = useState(false);
     useEffect(() => {
@@ -27,25 +30,62 @@ export default function AppLayout({
     }, []);
     return (
         <div className="min-h-screen grid grid-rows-[3rem_1fr] font-[family-name:var(--font-jetbrains-mono)] text-txt-light dark:text-txt-dark">
-            <header className="flex items-center justify-between py-2 px-4 sm:px-20 md:px-40">
-                <h1 className="text-2xl" onClick={() => router.push("/")}>
+            <header className="flex justify-between py-2 px-10 sm:px-20 md:px-32">
+                <h1
+                    className="text-2xl text-left cursor-pointer"
+                    onClick={() => router.push("/")}
+                >
                     Shadow.dev
                 </h1>
-                <div className="flex gap-8">
-                    <NavLink title="Projects" icon={<LaptopMinimal />} />
-                    <NavLink title="About me" icon={<FileUser />} />
-                    <NavLink title="Studies" icon={<GraduationCap />} />
-                    <NavLink title="Contact" icon={<Contact />} />
+                <div className="hidden lg:flex justify-center gap-8">
+                    <NavLink
+                        title="Projects"
+                        icon={<LaptopMinimal />}
+                        href="/projects"
+                    />
+                    <NavLink
+                        title="About me"
+                        icon={<FileUser />}
+                        href="/about"
+                    />
+                    <NavLink
+                        title="Studies"
+                        icon={<GraduationCap />}
+                        href="/studies"
+                    />
+                    <NavLink
+                        title="Contact"
+                        icon={<Contact />}
+                        href="/contact"
+                    />
                 </div>
-                <button
-                    ref={ref}
-                    className="cursor-pointer dark:text-white"
-                    onClick={() => {
-                        toggleSwitchTheme();
-                    }}
-                >
-                    {hasMounted && (isDarkMode ? <Moon /> : <Sun />)}
-                </button>
+                <div className="flex justify-end gap-4">
+                    <button
+                        ref={ref}
+                        className="cursor-pointer dark:text-white"
+                        onClick={() => {
+                            toggleSwitchTheme();
+                        }}
+                    >
+                        {hasMounted && (isDarkMode ? <Moon /> : <Sun />)}
+                    </button>
+                    <button
+                        className="lg:hidden"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        {isOpen ? <X /> : <Menu />}
+                    </button>
+                </div>
+
+                {/* Mobile Nav Menu */}
+                {isOpen && (
+                    <div className="absolute top-full left-0 w-full bg-background-light dark:bg-background-dark flex flex-col items-center gap-4 py-4 md:hidden shadow-lg border-t">
+                        <NavLink title="Projects" icon={<LaptopMinimal />} />
+                        <NavLink title="About me" icon={<FileUser />} />
+                        <NavLink title="Studies" icon={<GraduationCap />} />
+                        <NavLink title="Contact" icon={<Contact />} />
+                    </div>
+                )}
             </header>
             <main>
                 <section className="grid grid-rows-1 items-center justify-items-center h-full px-8 gap-16 sm:px-20 lg:px-40 font-[family-name:var(--font-jetbrains-mono)] dark:text-white">
