58307c1d4ccabeb0ecb460c2c337cd83a3fa0e0b
LxrdShadow <andritiana.idealy@gmail.com>
2025-07-06 15:43:14 +0300

    feat: add animation to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
 import PageAnimator from "@/layout/PageAnimator";
 
 export default function Home() {
@@ -44,8 +46,11 @@ export default function Home() {
     }, []);
 
     return (
-        <PageAnimator>
-            <h2 className="text-6xl font-bold sm:ml-8">
+        <PageAnimator animateElements={true}>
+            <motion.h2
+                variants={itemVariants}
+                className="text-6xl font-bold sm:ml-8"
+            >
                 Hey, I&apos;m{" "}
                 <span className="animate-gradient-rotate text-transparent bg-clip-text bg-gradient-to-tr from-accent-1 to-accent">
                     {name}
@@ -53,15 +58,21 @@ export default function Home() {
                 <span className="text-6xl animate-[simple-wave-animation_3s_infinite] md:animate-[wave-animation_3.3s_infinite] origin-[70%_70%] inline-block select-none">
                     👋
                 </span>
-            </h2>
+            </motion.h2>
 
-            {/*<span className="text-6xl origin-[70%_70%]">👋</span>*/}
-            <p className="text-lg max-w-xl mx-auto dark:text-gray-300 text-gray-600">
+            <motion.p
+                variants={itemVariants}
+                className="text-lg max-w-xl mx-auto dark:text-gray-300 text-gray-600"
+            >
                 CS student with a love-hate relationship with semicolons and
                 life itself. Passionate about web tech, machine learning,
                 security, and doing cool stuff with code.
-            </p>
-            <div className="grid grid-cols-2 gap-4">
+            </motion.p>
+
+            <motion.div
+                variants={itemVariants}
+                className="grid grid-cols-2 gap-4"
+            >
                 <Link
                     href="/about-me"
                     className="flex justify-center items-center outline-none px-6 py-2 mt-4 bg-txt-light dark:bg-txt-dark text-txt-dark dark:text-txt-light border border-txt-light dark:border-txt-dark rounded hover:bg-txt-dark dark:hover:bg-txt-light hover:text-txt-light dark:hover:text-txt-dark transition cursor-pointer"
@@ -74,7 +85,12 @@ export default function Home() {
                 >
                     <p>Let&apos;s Connect</p>
                 </Link>
-            </div>
+            </motion.div>
         </PageAnimator>
     );
 }
+
+const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeOut" } },
+} as const;
--- src/layout/PageAnimator.tsx
+++ src/layout/PageAnimator.tsx
@@ -2,22 +2,40 @@
 
 import { motion } from "framer-motion";
 
+const containerVariants = {
+    hidden: {},
+    show: {
+        transition: {
+            staggerChildren: 0.2,
+        },
+    },
+} as const;
+
 const PageAnimator = ({
     children,
+    animateElements = false,
 }: Readonly<{
     children: React.ReactNode;
+    animateElements?: boolean;
 }>) => {
-    return (
-        <motion.div
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.5 }}
-            className="flex-1 flex flex-col items-center justify-center text-center space-y-8"
-        >
-            {children}
-        </motion.div>
-    );
+    const divAttributes = {
+        exit: { opacity: 0, y: -10 },
+        transition: { duration: 0.4 },
+        ...(animateElements
+            ? {
+                  variants: containerVariants,
+                  initial: "hidden",
+                  animate: "show",
+              }
+            : {
+                  initial: { opacity: 0, y: 10 },
+                  animate: { opacity: 1, y: 0 },
+              }),
+        className:
+            "flex-1 flex flex-col items-center justify-center text-center space-y-8",
+    };
+
+    return <motion.div {...divAttributes}>{children}</motion.div>;
 };
 
 export default PageAnimator;
