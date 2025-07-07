42c51b0b97b2c429b86e4be0980074fed186d0ee
LxrdShadow <andritiana.idealy@gmail.com>
2025-07-07 11:36:54 +0300

    feat(projects): add the list of projects from a json file

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,23 @@
 import PageAnimator from "@/layout/PageAnimator";
+import ProjectCard from "@/components/ProjectCard";
+import projects from "@/data/projects.json";
+
+type Project = {
+    title: string;
+    description: string;
+    tech: string[];
+    link?: string;
+    imageSrc?: string;
+};
 
 const Projects = () => {
     return (
         <PageAnimator>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"></div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                {projects.map((project: Project) => (
+                    <ProjectCard key={project.title} {...project} />
+                ))}
+            </div>
         </PageAnimator>
     );
 };
