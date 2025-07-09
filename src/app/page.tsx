0c40812b544bc6864a0003ce70c5ef0fb6140fc2
LxrdShadow <andritiana.idealy@gmail.com>
2025-07-09 13:42:40 +0300

    fix: fix the button size bug in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { motion } from "framer-motion";
 
 import PageAnimator from "@/layout/PageAnimator";
 
+const MotionLink = motion(Link);
+
 export default function Home() {
     const [name, setName] = useState("Shadow");
 
@@ -73,22 +75,20 @@ export default function Home() {
                 variants={containerVariants}
                 className="grid grid-cols-2 gap-4"
             >
-                <motion.div variants={itemVariants} className="flex-1">
-                    <Link
-                        href="/about-me"
-                        className="flex justify-center items-center outline-none px-6 py-2 mt-4 bg-txt-light dark:bg-txt-dark text-txt-dark dark:text-txt-light border border-txt-light dark:border-txt-dark rounded hover:bg-txt-dark dark:hover:bg-txt-light hover:text-txt-light dark:hover:text-txt-dark cursor-pointer transition-all duration-300"
-                    >
-                        <p>My resume</p>
-                    </Link>
-                </motion.div>
-                <motion.div variants={itemVariants} className="flex-1">
-                    <Link
-                        href="/contact"
-                        className="flex justify-center items-center outline-none px-6 py-2 mt-4 border border-txt-light dark:border-txt-dark rounded hover:bg-bck-dark dark:hover:bg-bck-light hover:text-txt-dark dark:hover:text-txt-light cursor-pointer transition-all duration-300"
-                    >
-                        <p>Let&apos;s Connect</p>
-                    </Link>
-                </motion.div>
+                <MotionLink
+                    variants={itemVariants}
+                    href="/about-me"
+                    className="flex-1 flex justify-center items-center outline-none px-6 py-2 mt-4 bg-txt-light dark:bg-txt-dark text-txt-dark dark:text-txt-light border border-txt-light dark:border-txt-dark rounded hover:bg-txt-dark dark:hover:bg-txt-light hover:text-txt-light dark:hover:text-txt-dark cursor-pointer transition-colors duration-300"
+                >
+                    <p>My resume</p>
+                </MotionLink>
+                <MotionLink
+                    variants={itemVariants}
+                    href="/contact"
+                    className="flex-1 flex justify-center items-center outline-none px-6 py-2 mt-4 border border-txt-light dark:border-txt-dark rounded hover:bg-bck-dark dark:hover:bg-bck-light hover:text-txt-dark dark:hover:text-txt-light cursor-pointer transition-colors duration-300"
+                >
+                    <p>Let&apos;s Connect</p>
+                </MotionLink>
             </motion.div>
         </PageAnimator>
     );
@@ -100,7 +100,7 @@ const containerVariants = {
         opacity: 1,
         transition: {
             staggerChildren: 0.15,
-            delayChildren: 0.2,
+            delayChildren: 0.1,
         },
     },
 };
