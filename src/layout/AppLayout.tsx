9ef5f2c993f3a04c00d83c9fdbc589c85e65c40b
LxrdShadow <andritiana.idealy@gmail.com>
2025-07-24 19:13:20 +0300

    feat(ui): make only the main section scrollable and remove the scrollbar

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -10,19 +10,26 @@ export default function AppLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="min-h-dvh flex flex-col justify-between font-[family-name:var(--font-jetbrains-mono)] text-txt-light dark:text-txt-dark">
-            <div className="bg-animation absolute min-h-screen min-w-screen z-[-100]">
+        <div className="h-dvh flex flex-col font-[family-name:var(--font-jetbrains-mono)] text-txt-light dark:text-txt-dark overflow-hidden">
+            {/* Background stars */}
+            <div className="bg-animation absolute inset-0 z-[-100]">
                 <div id="stars"></div>
                 <div id="stars2"></div>
                 <div id="stars3"></div>
                 <div id="stars4"></div>
             </div>
+
+            {/* Header */}
             <Header />
-            <main>
-                <section className="grid grid-rows-1 items-center justify-items-center h-full p-8 gap-16 sm:px-20 lg:px-40 font-[family-name:var(--font-jetbrains-mono)] dark:text-white overflow-visible">
+
+            {/* Main content area */}
+            <main className="flex-1 overflow-y-auto scrollbar-none">
+                <section className="grid grid-rows-1 items-center justify-items-center min-h-full p-8 gap-16 sm:px-20 lg:px-40 font-[family-name:var(--font-jetbrains-mono)] dark:text-white">
                     <AnimatePresence mode="wait">{children}</AnimatePresence>
                 </section>
             </main>
+
+            {/* Footer */}
             <footer className="p-4 text-center text-sm text-gray-400">
                 © {new Date().getFullYear()} Shadow. All rights reserved.
             </footer>
